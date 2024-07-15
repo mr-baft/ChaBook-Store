416a215c7d046550ffc0eaa2a06b25775cc7de85
mr-baft <116699711+mr-baft@users.noreply.github.com>
2024-07-15 17:19:23 +0330

    Add files via upload

---
+++ AssetFiles/Script/main.js
@@ -0,0 +1,93 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== Menu Show =====*/
+/* Validate if constant exists */
+
+/*===== Hide Show =====*/
+/* Validate if constant exists */
+
+/*=============== IMAGE GALLERY ===============*/
+function imgGallery() {
+  const mainImg = document.querySelector('.datails-image');
+  const smallImg = document.querySelectorAll('.details-small-image');
+
+  smallImg.forEach((img) => {
+    img.addEventListener('click' , function() {
+      mainImg.src = this.src;
+    });
+  });
+}
+
+imgGallery();
+
+/*=============== SWIPER CATEGORIES ===============*/
+var swiperCategories = new Swiper(".categories-container", {
+    spaceBetween: 24,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 24,
+      },
+    },
+  });
+/*=============== SWIPER PRODUCTS ===============*/
+var swiperProducts = new Swiper(".recommend-container", {
+  spaceBetween: 24,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 5,
+      spaceBetween: 24,
+    },
+  },
+});
+
+/*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[content]')
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () =>{
+    const target=document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active-tab');
+    });
+
+    target.classList.add('active-tab');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('active-tab');
+    });
+
+    tab.classList.add('active-tab');
+  });
+});
